2ced606f8a762dc1cfc3d913a2683dbe5580f62e
sossost <shagrat8546@gmail.com>
2024-09-17 22:32:30 +0900

    feat : md렌더러 구현

---
+++ src/components/MarkdownRenderer.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  FlatList,
+  ScrollView,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
+import Markdown from "react-native-markdown-display";
+
+type MarkdownRendererProps = {
+  content: string;
+  style?: StyleProp<ViewStyle>;
+};
+
+const MarkdownRenderer = ({ content, style }: MarkdownRendererProps) => {
+  return (
+    <FlatList
+      style={[styles.container, style]}
+      data={content.split("\n")}
+      renderItem={({ item }) => (
+        <Markdown style={markdownStyles}>{item}</Markdown>
+      )}
+      keyExtractor={(item, index) => index.toString()}
+      ListFooterComponent={<View style={{ height: 32 }} />}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+const markdownStyles = StyleSheet.create({
+  body: {
+    color: "#333",
+    fontSize: 16,
+  },
+  heading1: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  heading2: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 8,
+  },
+  heading3: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 6,
+  },
+  link: {
+    color: "#0000FF",
+    textDecorationLine: "underline",
+  },
+  list_item: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+  },
+});
+
+export default MarkdownRenderer;

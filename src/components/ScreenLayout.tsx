1eeab09018a076ed2a3eeb107322f666fc161133
sossost <shagrat8546@gmail.com>
2024-09-17 22:33:34 +0900

    feat : 스크린 레이아웃 추가

---
+++ src/components/ScreenLayout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { Header, HeaderProps } from "./Header";
+
+type ScreenLayoutProps = {
+  children: React.ReactNode;
+  header?: HeaderProps;
+  style?: StyleProp<ViewStyle>;
+};
+
+export const ScreenLayout = ({
+  children,
+  header,
+  style,
+}: ScreenLayoutProps) => {
+  return (
+    <View style={[styles.container, style]}>
+      {header === null ? null : <Header {...header} />}
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingHorizontal: 18,
+  },
+});

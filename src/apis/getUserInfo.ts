5d176f1d38fa12868a40d98a7d6ca6ec43604c72
sossost <shagrat8546@gmail.com>
2024-09-17 22:33:52 +0900

    feat : api 초안 추가

---
+++ src/apis/getUserInfo.ts
@@ -0,0 +1,8 @@
+import { axiosInstance } from "./axios";
+
+export const getUserInfo = async () => {
+  // const response = await axiosInstance.get("/user/info");
+  // return response.data;
+
+  return Promise.resolve(null);
+};

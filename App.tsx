aa21c45fc65b8459da2f89d0211039cd24c2d257
sossost <shagrat8546@gmail.com>
2024-09-17 22:35:33 +0900

    refactor : app.tsx 리팩토링

--- App.tsx
+++ App.tsx
@@ -1,27 +1,15 @@
 import { ProviderGroup } from "providers";
-import { NavigationContainer } from "@react-navigation/native";
 import { CheckerGroup } from "checkers";
 import { StatusBar } from "react-native";
-import { useFonts } from "expo-font";
-import { Button, ErrorBoundary } from "components";
-import { SystemErrorScreen } from "screens";
+import { ErrorBoundary } from "components";
+import { SystemErrorScreen, Webview } from "screens";
 import SplashScreen from "react-native-splash-screen";
-import { usePushNotification } from "hooks";
-
-import { useContext, useEffect } from "react";
-import { AuthContext } from "providers/AuthContextProvider";
+import { usePretendardFont, usePushNotification } from "hooks";
+import { WEBVIEW_BASE_URL } from "constants/url";
 
 const App = () => {
   usePushNotification();
-  const [fontsLoaded] = useFonts({
-    "Pretendard-Regular": require("./assets/fonts/Pretendard-Regular.ttf"),
-    "Pretendard-Medium": require("./assets/fonts/Pretendard-Medium.ttf"),
-    "Pretendard-Bold": require("./assets/fonts/Pretendard-Bold.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
+  usePretendardFont();
 
   return (
     <ErrorBoundary
@@ -31,27 +19,14 @@ const App = () => {
         return <SystemErrorScreen onPress={reset} />;
       }}
     >
-      <NavigationContainer>
-        <ProviderGroup>
-          <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-          <CheckerGroup>
-            <Logout />
-            {/* <Webview url={WEBVIEW_BASE_URL} /> */}
-          </CheckerGroup>
-        </ProviderGroup>
-      </NavigationContainer>
+      <ProviderGroup>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <CheckerGroup>
+          <Webview url={WEBVIEW_BASE_URL} />
+        </CheckerGroup>
+      </ProviderGroup>
     </ErrorBoundary>
   );
 };
 
 export default App;
-
-const Logout = () => {
-  const { logout } = useContext(AuthContext);
-
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
-  return <Button onPress={logout} title="로그아웃" />;
-};

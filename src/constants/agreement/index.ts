8735fcc7c2c6f198efe4673c27220e8c1733c7a0
sossost <shagrat8546@gmail.com>
2024-09-17 22:32:58 +0900

    feat : 약관 상세 텍스트 추가

---
+++ src/constants/agreement/index.ts
@@ -0,0 +1,7 @@
+export * from "./termsOfUse";
+export * from "./privacyPolicy";
+export * from "./sensitiveInfo";
+export * from "./privacyThirdParty";
+export * from "./privacyTransfer";
+export * from "./age";
+export * from "./marketing";

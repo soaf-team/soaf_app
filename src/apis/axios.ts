d578a0519074a58a7914dab6a09f75646c4833e6
sossost <shagrat8546@gmail.com>
2024-10-27 21:44:36 +0900

    fix : api 베이스 url 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    "http://default-api-service-77559-26019998-ded2c8967f62.kr.lb.naverncp.com:8443/",
+  baseURL: "https://soaf.me/",
   headers: {
     Authorization: "Bearer api_secret_soap",
   },

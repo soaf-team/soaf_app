e81c8e160d95a819b494fb64b9b7fa6c57215c87
sossost <shagrat8546@gmail.com>
2024-11-01 16:15:56 +0900

    feat : 폰트 추가

--- src/hooks/usePretendardFont.ts
+++ src/hooks/usePretendardFont.ts
@@ -4,7 +4,10 @@ export const usePretendardFont = () => {
   const [fontsLoaded] = useFonts({
     "Pretendard-Regular": require("assets/fonts/Pretendard-Regular.ttf"),
     "Pretendard-Medium": require("assets/fonts/Pretendard-Medium.ttf"),
+    "Pretendard-SemiBold": require("assets/fonts/Pretendard-SemiBold.ttf"),
     "Pretendard-Bold": require("assets/fonts/Pretendard-Bold.ttf"),
+    "Pretendard-ExtraBold": require("assets/fonts/Pretendard-ExtraBold.ttf"),
+    "Pretendard-Black": require("assets/fonts/Pretendard-Black.ttf"),
   });
 
   if (!fontsLoaded) {

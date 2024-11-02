33945e464a623f9e1fbf92a6b91ddd833c577068
sossost <shagrat8546@gmail.com>
2024-11-02 16:44:12 +0900

    refactor : 로그인 함수 type safe하게 리팩토링

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -2,29 +2,60 @@ import { OauthType } from "types/global";
 import { axiosInstance } from "./axios";
 import { extractTokensFromCookies } from "utils";
 
-type LoginResponse = {
+type LoginResultCase = "login" | "join" | "sns";
+
+type BaseLoginResponse = {
   resultCase: LoginResultCase;
-  access?: string;
-  refresh?: string;
-  password?: string;
+};
+
+type JoinResponse = BaseLoginResponse & {
+  resultCase: "join";
+  password: string;
+};
+
+type LoginResponse = BaseLoginResponse & {
+  resultCase: "login";
+  access: string;
+  refresh: string;
+};
+
+type DuplicatedEmailResponse = BaseLoginResponse & {
+  resultCase: "sns";
+  existingSns: OauthType;
 };
 
 type APIResponse = {
-  data: LoginResponse;
+  data: JoinResponse | LoginResponse | DuplicatedEmailResponse;
   statusCode: number;
 };
-type LoginResultCase = "login" | "join" | "sns";
+
+type BaseLoginResult = {
+  resultCase: LoginResultCase;
+};
+
+type JoinResult = BaseLoginResult & {
+  resultCase: "join";
+  password: string;
+};
+
+type LoginResult = BaseLoginResult & {
+  resultCase: "login";
+  accessToken: string;
+  refreshToken: string;
+};
+
+type SNSLoginResult = BaseLoginResult & {
+  resultCase: "sns";
+  existingSns: OauthType;
+};
+
+type APIResult = JoinResult | LoginResult | SNSLoginResult;
 
 export const login = async (payload: {
   oAuthToken: string;
   email: string;
   oAuthType: OauthType;
-}): Promise<{
-  resultCase: LoginResultCase;
-  accessToken?: string;
-  refreshToken?: string;
-  password?: string;
-}> => {
+}): Promise<APIResult> => {
   const response = await axiosInstance.post<APIResponse>("user/sns-login", {
     email: payload.email,
     token: payload.oAuthToken,
@@ -32,24 +63,33 @@ export const login = async (payload: {
     name: "test",
   });
 
-  const { resultCase, password } = response.data.data;
+  const resultCase = response.data.data.resultCase;
 
-  if (resultCase === "join") {
-    return { resultCase, password };
+  switch (resultCase) {
+    case "join":
+      return {
+        resultCase,
+        password: response.data.data.password,
+      };
+    case "login":
+      const cookies = response.headers["set-cookie"];
+
+      if (!cookies) throw new Error("No cookies found in response headers");
+
+      const { accessToken, refreshToken } = extractTokensFromCookies(cookies);
+
+      if (!accessToken || !refreshToken)
+        throw new Error("No tokens found in cookies");
+
+      return {
+        resultCase,
+        accessToken: response.data.data.access,
+        refreshToken: response.data.data.refresh,
+      };
+    case "sns":
+      return {
+        resultCase,
+        existingSns: response.data.data.existingSns,
+      };
   }
-
-  const cookies = response.headers["set-cookie"];
-
-  if (!cookies) throw new Error("No cookies found in response headers");
-
-  const { accessToken, refreshToken } = extractTokensFromCookies(cookies);
-
-  if (!accessToken || !refreshToken)
-    throw new Error("No tokens found in cookies");
-
-  return {
-    resultCase,
-    accessToken,
-    refreshToken,
-  };
 };

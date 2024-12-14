71d374ce45c36ed70cf12d50d326e464f0aba675
Yoonjae Choi <dbswo9795@gmail.com>
2024-12-14 21:34:13 +0900

    feat: 감정 선택 컴포넌트 구현 (#6)
    * chore: 아이콘 추가
    
    * feat: 감정 스태틱 추가
    
    * feat: 감정 선택 컴포넌트 구현

---
+++ src/constants/mood.ts
@@ -0,0 +1,23 @@
+export const MOOD_RATINGS = {
+  level1: require('assets/level/level1.png'),
+  level2: require('assets/level/level2.png'),
+  level3: require('assets/level/level3.png'),
+  level4: require('assets/level/level4.png'),
+  level5: require('assets/level/level5.png'),
+} as const;
+
+export const MOOD_RATING_ARRAY = [
+  MOOD_RATINGS.level1,
+  MOOD_RATINGS.level2,
+  MOOD_RATINGS.level3,
+  MOOD_RATINGS.level4,
+  MOOD_RATINGS.level5,
+];
+
+export const MOOD_RATING_COLORS = [
+  '#848484',
+  '#D2D9D9',
+  '#BBE3EF',
+  '#6FD8F5',
+  '#48A4F8',
+];
---
+++ src/screens/diary/components/DailyRatingWidget.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { View, Image, StyleSheet, Animated, Pressable } from 'react-native';
+import { MOOD_RATING_ARRAY } from 'constants/mood';
+
+type MoodRating = 1 | 2 | 3 | 4 | 5;
+
+type DailyRatingWidgetProps = {
+  selectedRating: MoodRating | null;
+  handleSelectRating: (index: MoodRating | null) => void;
+};
+
+export const DailyRatingWidget = ({
+  selectedRating,
+  handleSelectRating,
+}: DailyRatingWidgetProps) => {
+  const animatedScales = MOOD_RATING_ARRAY.map(() => new Animated.Value(1));
+
+  const handlePressIn = (index: number) => {
+    Animated.spring(animatedScales[index], {
+      toValue: 1.1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = (index: number) => {
+    Animated.spring(animatedScales[index], {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  return (
+    <View style={styles.container}>
+      {MOOD_RATING_ARRAY.map((level, index) => {
+        const isSelected = selectedRating === index + 1;
+
+        return (
+          <Pressable
+            key={index}
+            onPressIn={() => handlePressIn(index)}
+            onPressOut={() => handlePressOut(index)}
+            onPress={() => handleSelectRating((index + 1) as MoodRating)}
+          >
+            <Animated.View
+              style={[
+                styles.imageContainer,
+                {
+                  transform: [{ scale: animatedScales[index] }],
+                },
+              ]}
+            >
+              <Image
+                source={level}
+                style={[styles.image, { opacity: isSelected ? 1 : 0.3 }]}
+                resizeMode="contain"
+              />
+            </Animated.View>
+          </Pressable>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    gap: 12,
+  },
+  imageContainer: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});

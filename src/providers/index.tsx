07438e79a18775e6ec1dfda548df08767719c2b9
jang_yoonsu <shagrat8546@gmail.com>
2024-12-29 18:40:19 +0900

    feat : 쿼리클라이언트 프로바이더 추가

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,17 +2,22 @@ import { ReactNode } from "react";
 import { SafeAreaProvider } from "./SafeAreaProvider";
 import { AuthContextProvider } from "./AuthContextProvider";
 import { NavigationProvider } from "./NavigationProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 type Props = {
   children: ReactNode;
 };
 
 export const ProviderGroup = ({ children }: Props) => {
+  const queryClient = new QueryClient();
+
   return (
     <NavigationProvider>
-      <AuthContextProvider>
-        <SafeAreaProvider>{children}</SafeAreaProvider>
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <SafeAreaProvider>{children}</SafeAreaProvider>
+        </AuthContextProvider>
+      </QueryClientProvider>
     </NavigationProvider>
   );
 };

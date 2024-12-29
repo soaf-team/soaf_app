7b817a4ed156e2c43e0c0f05fcf302b5c8dada51
jang_yoonsu <shagrat8546@gmail.com>
2024-12-29 18:24:26 +0900

    feat : 쿼리키 추가

---
+++ src/constants/queryKey.ts
@@ -0,0 +1,35 @@
+export const QUERY_KEY = {
+  userProfile: "userProfile",
+
+  currentUserDiaryList: "currentUserDiaryList",
+  FRIEND_DIARY_LIST: "friendDiaryList",
+  diaryDetail: "diaryDetail",
+
+  CURRENT_USER: "currentUser",
+
+  DIARY: "diary",
+  DIARY_DETAIL: "diaryDetail",
+  DIARY_STATS: "diaryStats",
+  MY_DIARY: "myDiary",
+  MY_DIARY_BY_MONTH: "myDiaryByMonth",
+
+  FRIEND_LIST: "friendList",
+
+  MY_HOME_DATA: "myHomeData",
+  OTHER_USER_HOME_DATA: "otherUserHomeData",
+  META_DIARY: "metaDiary",
+  SIMILAR_USER: "similarUser",
+  MY_MUSIC_LIST: "myMusicList",
+  MY_MUSIC_DETAIL: "myMusicDetail",
+  MY_MOVIE_LIST: "myMovieList",
+  MY_MOVIE_DETAIL: "myMovieDetail",
+  MY_BOOK_LIST: "myBookList",
+  MY_BOOK_DETAIL: "myBookDetail",
+  MY_YOUTUBE_LIST: "myYoutubeList",
+  MY_YOUTUBE_DETAIL: "myYoutubeDetail",
+
+  BLOCK_USER_LIST: "blockUserList",
+  SENT_SOAF_REQUEST_LIST: "sentSoafRequestList",
+  VALIDATE_USER_NAME: "validateUserName",
+  NOTIFICATION_LIST: "notificationList",
+};

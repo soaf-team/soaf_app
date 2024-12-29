86a0294683b674a42bc3232ee41309c6a236556d
Sam Choi <dbswo9795@gmail.com>
2024-12-29 11:20:10 +0900

    chore: 아이콘 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,17 @@
 import XIcon from './x-icon.svg';
 import ArrowIcon from './arrow-icon.svg';
+import LockIcon from './lock.svg';
+import UnlockIcon from './unlock.svg';
+import UnlockBlackIcon from './unlock-black.svg';
+import LockBlackIcon from './lock-black.svg';
+import AlbumIcon from './album.svg';
 
-export { XIcon, ArrowIcon };
+export {
+  XIcon,
+  ArrowIcon,
+  LockIcon,
+  UnlockIcon,
+  LockBlackIcon,
+  UnlockBlackIcon,
+  AlbumIcon,
+};

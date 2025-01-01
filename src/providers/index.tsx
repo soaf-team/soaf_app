bb82969a911d5e10dcbcd086298429f07afed907
sossost <shagrat8546@gmail.com>
2025-01-01 14:54:34 +0900

    feat : 제스쳐 핸들러, 바텀싯 모달 프로바이더 추가

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -3,6 +3,8 @@ import { SafeAreaProvider } from "./SafeAreaProvider";
 import { AuthContextProvider } from "./AuthContextProvider";
 import { NavigationProvider } from "./NavigationProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
 type Props = {
   children: ReactNode;
@@ -12,12 +14,16 @@ export const ProviderGroup = ({ children }: Props) => {
   const queryClient = new QueryClient();
 
   return (
-    <NavigationProvider>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <SafeAreaProvider>{children}</SafeAreaProvider>
-        </AuthContextProvider>
-      </QueryClientProvider>
-    </NavigationProvider>
+    <GestureHandlerRootView>
+      <BottomSheetModalProvider>
+        <NavigationProvider>
+          <QueryClientProvider client={queryClient}>
+            <AuthContextProvider>
+              <SafeAreaProvider>{children}</SafeAreaProvider>
+            </AuthContextProvider>
+          </QueryClientProvider>
+        </NavigationProvider>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
